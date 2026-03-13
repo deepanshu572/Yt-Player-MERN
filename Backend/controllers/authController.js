2d9104bf9ca6d003a8771387d85ba6c1e792fdc8
Deepanshu kumar <142653242+deepanshu572@users.noreply.github.com>
2026-03-13 13:08:23 +0530

    Fix cookie options in authController
    Updated cookie options to use 'sameSite' and added 'path'.

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -37,7 +37,8 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      samesite: "none",
+      sameSite: "none",
+      path : "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
@@ -63,9 +64,10 @@ export const login = async (req, res) => {
     }
     let token = await genToken(existing._id);
     res.cookie("token", token, {
-       httpOnly: true,
+      httpOnly: true,
       secure: true,
-      samesite: "none",
+      sameSite: "none",
+      path : "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(existing);
